94d574af48c1cb2091ed93b68615e0d08d58673f
Keith <hnefatl@gmail.com>
2022-09-26 19:50:44 +0200

    Add simple (non-interrupting so vulnerable to sleeps) button implementation

--- src/main.rs
+++ src/main.rs
@@ -69,29 +69,60 @@ fn sleep(seconds: f32) {
     asm::delay((seconds * 8_000_000f32) as u32)
 }
 
+struct Button {
+    pin: gpio::Pin<gpio::Gpioa, gpio::Ux, gpio::Input>,
+    handled: bool
+}
+impl Button {
+    fn new(pin: gpio::Pin<gpio::Gpioa, gpio::Ux, gpio::Input>) -> Button {
+        Button { pin, handled: false }
+    }
+
+    fn is_pressed(&self) -> bool {
+        into_ok(self.pin.is_high())
+    }
+    fn handle_pressed(&mut self) -> bool {
+        if self.is_pressed() {
+            if !self.handled  {
+                self.handled = true;
+                return true
+            }
+        } else if !self.is_pressed() {
+            if self.handled {
+                self.handled = false
+            }
+        }
+        return false;
+    }
+}
+struct Buttons {
+    user: Button,
+}
+impl Buttons {
+    fn new(mut gpioa: gpio::gpioa::Parts, _exti: &mut pac::EXTI) -> Buttons {
+        Buttons {
+            user: Button::new(gpioa.pa0.into_pull_down_input(&mut gpioa.moder, &mut gpioa.pupdr).downgrade())
+        }
+    }
+}
+
 #[entry]
 fn main() -> ! {
-    let peripherals = pac::Peripherals::take().unwrap();
+    let mut peripherals = pac::Peripherals::take().unwrap();
 
     let mut reset_and_clock_control = peripherals.RCC.constrain();
     let gpioe = peripherals.GPIOE.split(&mut reset_and_clock_control.ahb);
     let mut led_wheel = LEDWheel::new(gpioe);
 
-    let mut gpioa = peripherals.GPIOA.split(&mut reset_and_clock_control.ahb);
-    let user_button = gpioa.pa0.into_pull_down_input(&mut gpioa.moder, &mut gpioa.pupdr);
+    let gpioa = peripherals.GPIOA.split(&mut reset_and_clock_control.ahb);
+    let mut buttons = Buttons::new(gpioa, &mut peripherals.EXTI);
 
     // Start here so that we loop round to 0 on the first iteration.
     let mut index: usize = LEDWheel::COUNT - 1;
     let mut delta: i8 = 1;
-    let mut button_is_pressed = false;
     loop {
-        if user_button.is_high().unwrap_or(false) {
-            if !button_is_pressed {
-                button_is_pressed = true;
-                delta *= -1;
-            }
-        } else {
-            button_is_pressed = false;
+        if buttons.user.handle_pressed() {
+            delta *= -1;
         }
         let next_index = ((index as i8 + delta) % LEDWheel::COUNT as i8) as usize;
 
